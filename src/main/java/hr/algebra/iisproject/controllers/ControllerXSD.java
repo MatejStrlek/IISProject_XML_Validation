8e6c90a39eb1dcd8bcea371f206a68502c048e12
mgalic <matej.galic.ets@gmail.com>
2024-05-06 18:07:31 +0200

    unused imports

--- src/main/java/hr/algebra/iisproject/controllers/ControllerXSD.java
+++ src/main/java/hr/algebra/iisproject/controllers/ControllerXSD.java
@@ -12,12 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 import utils.XmlUtils;
 
-import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBException;
-import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.IOException;
 import java.io.InputStream;
